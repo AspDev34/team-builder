c4ea072cc15b24590dcd8c755a35269a9c67ef8d
Ike Otu <ikeotu@msn.com>
2020-01-29 16:25:05 -0800

    Initial commit, data set up

---
+++ team-builder/src/TeamData.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+[
+
+{
+    Name: 'Daffy Duck',
+    id: '123',
+    email: 'daffyduck@looneytunes.com',
+    Role: 'Dark Wing Duck'
+},
+
+{
+    Name: 'Clark Kent',
+    id: '456',
+    email: 'ckent@dailyplanet.com',
+    Role: 'Reporter'
+},
+
+{
+    Name: 
+}
+
+
+]
